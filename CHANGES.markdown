848101c5204ab536cb3a62d6c5e8f9fb6ea38597
jeffrey rosenbluth <jeffrey.rosenbluth@gmail.com>
2013-12-06 12:02:30 -0500

    added Changed log

---
+++ CHANGES.markdown
@@ -0,0 +1,6 @@
+0.1.0.1
+--------
+
+  * define Kolor in Types module
+
+0.1.0.0: Initial release
---
+++ src/Data/Colour/Palette/Types.hs
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Palette.Ty[es
+-- Copyright   :  (c) 2013 Jeffrey Rosenbluth
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  jeffrey.rosenbluth@gmail.com
+--
+-- Types used in Palette modules
+--
+-----------------------------------------------------------------------------
+
+module Data.Colour.Palette.Types
+       (
+         Kolor
+       ) where
+
+import Data.Colour
+
+type Kolor = Colour Double
